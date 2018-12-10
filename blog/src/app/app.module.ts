764f22d2c2d08ec121206dc3a329a42276e6721f
Przemek <przemekcichon97@gmail.com>
2018-12-10 18:16:39 +0100

    routing

--- blog/src/app/app.module.ts
+++ blog/src/app/app.module.ts
@@ -7,6 +7,28 @@ import { ContactComponent } from './components/contact/contact.component';
 import { MapComponent } from './components/map/map.component';
 import { QuizComponent } from './components/quiz/quiz.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import {Routes} from "@angular/router";
+import {RouterModule} from '@angular/router';
+
+
+const appRoutes: Routes = [
+  {
+    path: 'contact',
+    component: ContactComponent,
+  },
+  {
+    path: 'map',
+    component: MapComponent,
+  },
+  {
+    path: 'quiz',
+    component: QuizComponent,
+  },
+  {
+    path: 'nav-bar',
+    component: NavBarComponent,
+  }];
+
 
 @NgModule({
   declarations: [
@@ -18,9 +40,12 @@ import { NavBarComponent } from './components/nav-bar/nav-bar.component';
     NavBarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
